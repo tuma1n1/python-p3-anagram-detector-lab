1a83b8de2e8338529415a2f885b0234ef77dfbc6
Tumaini <tumainikebaya.234@gmail.com>
2024-07-23 11:36:57 +0300

    Adds anagram.py

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
-# your code goes here!
+# lib/anagram.py
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = "".join(sorted(self.word))
+
+    def match(self, word_list):
+        return [word for word in word_list if "".join(sorted(word.lower())) == self.sorted_word]
